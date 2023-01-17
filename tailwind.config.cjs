3572410a9a8a50b02fab80429dd2a0f6d05e371c
warayut <ch.warayuth@gmail.com>
2023-01-17 11:43:18 +0700

    Async Thunk Function - Adding a Skeleton Loader with TailwindCSS

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        shimmer: {
+          '100%': { transform: 'translateX(100%)' },
+        },
+      },
+      animation: {
+        shimmer: 'shimmer 1.5s infinite',
+      },
+    },
   },
   plugins: [],
-}
+};
