2c1a254cb1ab208bc0cd7f6f7d1613baa9dd7c3d
warayut <ch.warayuth@gmail.com>
2023-01-17 14:06:30 +0700

    Async Thunk Function - Creating a Reusable Thunk Hook

--- src/store/thunks/adduser.js
+++ src/store/thunks/adduser.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { faker } from '@faker-js/faker';
 
 const addUser = createAsyncThunk('users/add', async () => {
-  const response = await axios.post('ttp://localhost:3005/users', {
+  const response = await axios.post('http://localhost:3005/users', {
     name: faker.name.fullName(),
   });
 
